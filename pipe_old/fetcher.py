2ab36a6685cf19ad4a1dbd704c3cbe0bd915722c
Prajwal Dhuli <prajwaldhuli88@gmail.com>
2026-04-13 18:33:08 +0100

    Added fetch timestamp and error handling to fetcher.py

--- pipe_old/fetcher.py
+++ pipe_old/fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 COINGECKO_URL = "https://api.coingecko.com/api/v3/coins/markets"
 
@@ -13,11 +14,30 @@ def fetch_coins(vs_currency="usd", per_page=20):
         "price_change_percentage": "7d"
     }
 
-    response = requests.get(COINGECKO_URL, params=params)
+    try:
+    
+        response = requests.get(COINGECKO_URL, params=params)
 
-    if response.status_code == 200:
-        print(f"Successfully fetched {len(response.json())} coins")
-        return response.json()
-    else:
-        print(f"Error fetching data: {response.status_code}")
+        if response.status_code == 200:
+            data = response.json()
+            fetched_at = datetime.utcnow().isoformat()
+            print(f"Successfully fetched {len(data)} coins at {fetched_at}")
+            return data
+        
+        elif response.status_code == 429:
+            print("Rate limited by CoinGecko try again in 60 seconds")
+            return[]
+        
+        else:
+            print(f"Error fetching data: {response.status_code}")
+            return []
+        
+    except requests.exceptions.Timeout:
+        print("Request timed out — CoinGecko took too long to respond")
         return []
+    
+    except requests.exceptions.ConnectionError:
+        print("No internet connection — please check your network")
+        return []
+
+
